f5d61c1fb220ae1568163adca8308b84da59d99c
MHAN001 <hanxu.workbox@gmail.com>
2019-11-22 21:22:49 -0500

    add ngx-charts

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -4,15 +4,24 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-front/src/app/dashboard/dashboard.component.ts
+++ angular-front/src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,39 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { single } from './data';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
+    single: any[];
+    multi: any[];
 
-  constructor() {
-  }
+    view: any[] = [700, 400];
 
-  ngOnInit() {
-  }
+    // options
+    showXAxis = true;
+    showYAxis = true;
+    gradient = false;
+    showLegend = true;
+    showXAxisLabel = true;
+    xAxisLabel = 'Country';
+    showYAxisLabel = true;
+    yAxisLabel = 'Population';
+
+    colorScheme = {
+      domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    };
+
+    constructor() {
+      Object.assign(this, { single })
+    }
+
+    OnSelect(event) {
+      console.log(event);
+    }
 
 }

ba5dd63c3152073492e296d08b1da3a59023345d
MHAN001 <hanxu.workbox@gmail.com>
2019-11-25 18:41:33 -0500

    All front charts and color

--- angular-front/src/app/dashboard/dashboard.component.ts
+++ angular-front/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,5 @@
-import { Component, NgModule} from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { single } from './data';
+import { Component} from '@angular/core';
+import { single, multi } from './data';
 
 @Component({
     selector: 'app-dashboard',
@@ -14,26 +12,25 @@ export class DashboardComponent {
 
     view: any[] = [700, 400];
 
-    // options
+    // pie & bar
     showXAxis = true;
     showYAxis = true;
-    gradient = false;
+    gradient = true;
     showLegend = true;
     showXAxisLabel = true;
     xAxisLabel = 'Country';
     showYAxisLabel = true;
     yAxisLabel = 'Population';
 
+    //pie charts
+    animations = true;
+    labels = true;
+
     colorScheme = {
-      domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+      domain: ['#1E1F35', '#5779C8', '#FFC64C', '#FF6737', '#E75047']
     };
 
     constructor() {
-      Object.assign(this, { single })
+      Object.assign(this, { single });
     }
-
-    OnSelect(event) {
-      console.log(event);
-    }
-
 }
